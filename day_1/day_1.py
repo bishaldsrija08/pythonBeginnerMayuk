515c4709a9c749f16ed733a673dc027ebc5b0757
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-12-19 19:42:55 +0545

    Add initial print statements demonstrating basic Python syntax and operations

---
+++ day_1/day_1.py
@@ -0,0 +1,16 @@
+print("My name is Bishal Rijal.") # Dobule quoted because of apostrophes.
+print(120) # Not double quoted because it's an integer.
+print("123544") # Double quoted because it's a string of numbers.
+print("I am good boy")
+print(45+78) # Not double quoted because it's a mathematical operation.
+print(45/6)
+print(4*5) # * Multiplication
+print(45-9) # - Subtraction
+print(10%3) # % Modulus - Helps to find the remainder.
+print(10//3) # // Floor Division - Helps to find the quotient without decimal.
+print(2**3) # ** Exponentiation - Helps to find the power. 2*2*2 = 8
+print("Hello " + "World") # String Concatenation
+print("Bishal "+ "Rijal")
+print("Hello " * 3) # String Repetition
+print("Hello 1\nWorld 2") # \n New Line
+print("Hello 1\tWorld 2") # \t Tab Space
