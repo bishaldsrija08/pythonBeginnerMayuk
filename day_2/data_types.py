8b49232ef6258cdcfddc87f659f07917d6805f0b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-12-22 19:22:19 +0545

    Add data type demonstrations and calculations for area, perimeter, and circumference

---
+++ day_2/data_types.py
@@ -0,0 +1,21 @@
+print(90)
+print(3.14)
+print(90.0) # Equivalent to 90
+
+print("Hello, World!")
+print("bishal123")
+
+print(11+11)
+print("11"+"11")  # Concatenation of strings
+print("Bishal "+ "Rijal")
+
+# print("11"+11)
+
+num_1 = 90
+num_2= 92
+print(num_1 + num_2)
+
+name = "Bishal"
+lname = "Rijal"
+
+print(name + " " + lname)
---
+++ day_2/practice.py
@@ -0,0 +1,29 @@
+# Calculate area and perimeter of a rectangle
+length = 5
+breadth = 10
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("Area of rectangle:", area)
+print("Perimeter of rectangle:", perimeter)
+
+# Calculate area and circumference of a circle.
+radius = 7
+pi = 3.14
+
+area_circle = pi * radius * radius
+circumference = 2 * pi * radius
+
+print("Area of circle:", area_circle)
+print("Circumference of circle:", circumference)
+
+# Calculate simple interest.
+# principal, rate, time
+principal = 1000
+rate = 5
+time = 3
+#  si = (P * R * T) / 100
+#  display si
+
+
+# WAP to calculate the perimeter and area of a square.
