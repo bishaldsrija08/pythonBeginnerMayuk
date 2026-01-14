fa05d17d85d9ee3140bc5476f47bc13ae5fc1fe0
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-14 20:03:49 +0545

    Add swap function to demonstrate variable swapping in Python

---
+++ questionsToSolve/swap.py
@@ -0,0 +1,13 @@
+a= int(input("Enter the value of a: "))
+b= int(input("Enter the value of b: "))
+
+# Swapping the values of a and b
+a, b = b,a
+# temp =a
+# a=b
+# b=temp
+
+
+
+print("The value of a after swapping: ", a)
+print("The value of b after swapping: ", b)
