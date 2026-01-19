be5e51f57033745e07e3a72c5546e57983816055
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-19 19:09:58 +0545

    Add while loop exercises and example scripts for basic programming tasks

---
+++ day_6/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+j = 10 # inicializacion
+while j>=1:
+    print(j, end=" ")
+    j = j-1 # decrement
---
+++ day_6/while.py
@@ -0,0 +1,26 @@
+
+# for i in range(1, 100, 1):
+    # // bos
+
+
+
+"""
+inicializacion
+condition
+increment/decrement
+"""
+
+# WAP to print numbers from 1 to 10 using while loop
+
+i = 1 # inicializacion
+
+while i<=10:
+    print(i, end=" ")
+    i = i+1 # increment
+
+# WAP to print numbers from 10 to 1 using while loop
+
+j = 10 # inicializacion
+while j>=1:
+    print(j, end=" ")
+    j = j-1 # decrement
