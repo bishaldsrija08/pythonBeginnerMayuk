ddffc1ae37382e42540e1a1f24ecee9d931c5694
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-21 19:16:08 +0545

    Add nested loops examples for star patterns and number sequences

---
+++ day_7/nested.py
@@ -0,0 +1,50 @@
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+lines = 5
+for i in range(1, lines+1): # outer loop for number of lines
+    for j in range(1, i+1): # inner loop for printing stars
+        print("*", end=" ")
+    print()
+    
+"""
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
+lines = 5
+for i in range(lines, 0, -1): # outer loop for number of lines
+    for j in range(1, i+1): # inner loop for printing stars
+        print("*", end=" ")
+    print() # move to the next line
+    
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+lines = 5
+for i in range(1, lines+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+
+"""
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+"""
