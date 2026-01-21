7ac55c1130ad8b9dc14e8ffc76c0911b57bd09da
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-21 19:36:27 +0545

    Add descending order number patterns and update example scripts

--- day_7/nested.py
+++ day_7/nested.py
@@ -62,3 +62,24 @@ for i in range( lines, 0, -1):
 1 2
 1
 """
+
+lines = 5
+for i in range(lines, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+"""
+
+"""
+* * * * *
+*       *
+*       *
+* * * * *
+"""
