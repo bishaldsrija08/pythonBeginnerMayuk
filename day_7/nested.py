9f1ac093bc8eadd951cd67ab266c33627c75d490
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-23 19:26:30 +0545

    Add border star pattern examples and update tempCodeRunnerFile

--- day_7/nested.py
+++ day_7/nested.py
@@ -91,3 +91,13 @@ for i in range(1, lines+1):
 *       *
 * * * * *
 """
+
+lines= 4
+
+for i in range(1, lines+1):
+    for j in range(1, lines+1):
+        if i==1 or i==lines or j==1 or j==lines: # condition for border stars [logic is : first row, last row, first column, last column]
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
--- day_7/tempCodeRunnerFile.py
+++ day_7/tempCodeRunnerFile.py
@@ -1,7 +1,9 @@
-lines = 5
-count = 1
+lines= 4
+
 for i in range(1, lines+1):
-    for j in range(1, i+1):
-        print(count, end= " ")
-        count = count+1
+    for j in range(1, lines+1):
+        if i==1 or i==lines or j==1 or j==lines:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
     print()
